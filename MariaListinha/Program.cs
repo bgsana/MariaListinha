3599fce8402def993720e3a61d873e9d5ef940a9
bgsana <bgs.ana08@gmail.com>
2026-03-27 21:49:36 -0300

    Configured database connection and application services

--- MariaListinha/Program.cs
+++ MariaListinha/Program.cs
@@ -4,23 +4,44 @@ using MariaListinha.Data;
 using MariaListinha.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+// Serviço de conexão com o banco de dados
+string conexao = builder.Configuration.GetConnectionString("Conexao");
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+// Serviço de configuração do Identity - Gestão de Usuários
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+    opt =>
+    {
+        opt.User.RequireUniqueEmail = true;
+        opt.SignIn.RequireConfirmedAccount = false;
+    }
+)
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Garante a existência do banco
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext =
+    scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -29,6 +50,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
